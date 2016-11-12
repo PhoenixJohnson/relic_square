11803bce1e067ba9836f5629939592d41e05a22b
PhoenixJohnson <yosku@163.com>
2016-11-12 23:11:29 +0800

    update

--- src/main/java/com/relic/square/exception/RelicException.java
+++ src/main/java/com/relic/square/exception/RelicException.java
@@ -1,8 +1,7 @@
 package com.relic.square.exception;
 
-import com.ebay.globalenv.util.StringUtils;
-import org.ebayopensource.ginger.common.types.ErrorMessage;
 
+import org.springframework.util.StringUtils;
 
 public class RelicException extends Exception {
 
@@ -12,7 +11,7 @@ public class RelicException extends Exception {
      */
     private static final long serialVersionUID = -8198402697931984140L;
 
-    private ErrorMessage originalError;
+
     private String errorCode;
 
     private void setErrorCode(String errorCode) {
@@ -23,9 +22,6 @@ public class RelicException extends Exception {
         super();
     }
 
-    public RelicException(String message) {
-        super(message);
-    }
 
     public RelicException(String message, Throwable cause) {
         super(message, cause);
@@ -41,18 +37,11 @@ public class RelicException extends Exception {
         this.errorCode = errorCode;
     }
 
-    public RelicException(String message, ErrorMessage originalError) {
+    public RelicException(String message) {
         super(message);
-        this.setOriginalError(originalError);
     }
 
-    public ErrorMessage getOriginalError() {
-        return originalError;
-    }
 
-    public void setOriginalError(ErrorMessage originalError) {
-        this.originalError = originalError;
-    }
 
     public String getErrorCode() {
         return StringUtils.isEmpty(this.errorCode) ? getDefaultErrorCode() : this.errorCode;
